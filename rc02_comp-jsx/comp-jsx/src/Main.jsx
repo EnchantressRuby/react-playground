fde52465e110436fc3fb7f144d263e91ed26ac3d
Ruby <rmyskhrmn584@gmail.com>
2024-08-31 13:28:14 +0200

    styling

--- rc02_comp-jsx/comp-jsx/src/Main.jsx
+++ rc02_comp-jsx/comp-jsx/src/Main.jsx
@@ -12,16 +12,39 @@
 //! External CSS
 //! CSS files can be import directly. No export needed.
 //? CSS files can be added as import "path"
+import "./Main.css";
 
-import "./Main.css"
+import landscape from "./images/landscape.webp"
+
+
+
+
+
+const headerStyle = {
+    color: "teal",
+    backgroundColor: "lightgray",
+    fontWeight: "bold"
+};
 
 const Main = () => {
-    return(
+    return (
         <main>
-            <h2>Main Section</h2>
+            {/* JSX */}
+            {/* Inline CSS, (key-value), camelCase */}
+            <h2 style={{ color: "brown", backgroundColor: "lightcoral" }}>Main Section</h2>
+            <h3 style={headerStyle}>Other Header</h3>
             <p className="par">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Assumenda, ducimus!</p>
             <p className="par2">Lorem ipsum dolor sit amet.
             </p>
+            <div>
+                <img className="img" src="https://cdn.pixabay.com/photo/2024/02/23/18/59/desert-8592629_1280.jpg" alt="view" />
+
+                {/* local */}
+                <img className="img" src={landscape} alt="landscape" />
+
+                {/* public folder */}
+                <img className="img" src="./landscape2.webp" alt="landscape2" />
+            </div>
         </main>
     );
 };
