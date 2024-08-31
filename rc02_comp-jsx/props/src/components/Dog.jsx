7fb3192668206e1be0fce6ddd9be2dc3f36ee3e4
Ruby <rmyskhrmn584@gmail.com>
2024-08-31 14:32:57 +0200

    styling

--- rc02_comp-jsx/props/src/components/Dog.jsx
+++ rc02_comp-jsx/props/src/components/Dog.jsx
@@ -1,9 +1,25 @@
-const Dog = (props) => {
+import Name from "./Name"
+
+// const Dog = (props) => {
+//     return (
+//         <div>
+//             <p style={{padding: "1rem", fontWeight: "bold", fontSize: "2rem"}}>{props.name}</p>
+//             <img className="img" src={props.img} alt="" />
+//             <p style={{ backgroundColor: props.color, color: "white", padding: "1rem"}}>Color:{props.color}</p>
+//             <hr />
+//         </div>
+//     )
+// }
+
+const Dog = ({ name, img, color }) => {
+    //? dest
+    // const { name, img, color } = props
     return (
         <div>
-            <p style={{padding: "1rem", fontWeight: "bold", fontSize: "2rem"}}>{props.name}</p>
-            <img className="img" src={props.img} alt="" />
-            <p style={{ backgroundColor: props.color, color: "white", padding: "1rem"}}>Color:{props.color}</p>
+            <Name name={name} />
+            {/* <p style={{ padding: "1rem", fontWeight: "bold", fontSize: "2rem" }}>{name}</p> */}
+            <img className="img" src={img} alt="" />
+            <p style={{ backgroundColor: color, color: "white", padding: "1rem" }}>Color:{color}</p>
             <hr />
         </div>
     )
---
+++ rc02_comp-jsx/props/src/components/Name.jsx
@@ -0,0 +1,9 @@
+const Name = ({ name }) => {
+    return (
+        <div>
+            <h2 style={{ padding: "1rem", fontWeight: "bold", fontSize: "2rem" }}>Name: {name}</h2>
+        </div>
+    )
+}
+
+export default Name;
