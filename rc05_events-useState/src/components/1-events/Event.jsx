79ddf1ef5695d91619acdd7b2b795cb63668679f
Ruby <rmyskhrmn584@gmail.com>
2024-09-02 23:43:06 +0200

    events

--- rc05_events-useState/src/components/1-events/Event.jsx
+++ rc05_events-useState/src/components/1-events/Event.jsx
@@ -1,17 +1,34 @@
 const Event = () => {
-
+    //?local variable
+    let message = "Bye bye!";
+    
     //? Event handler func
 
     const handleClick = (event) => {
         alert("Clicked!")
         console.log(event);
     }
-    
+
+    function handleExt(msg) {
+        console.log(msg);
+    }
+
+    const handleChng = () => {
+        message = "Hello Friends";
+        console.log(message);
+        //! Direct DOM intervention is not preferred in React
+        // document.querySelector("p").textContent = message;
+    };
+
     return (
         <div>
             <h2>Events</h2>
             <button onClick={handleClick} id="myBtn" name="click" className="btn btn-primary">Click me</button>
-            <button onClick={()=>alert("Deleted!")} className="btn btn-danger">Delete</button>
+            <button onClick={() => alert("Deleted!")} className="btn btn-danger">Delete</button>
+            {/* <button onClick={() => handleExt("Bye!")} className="btn btn-success">Exit</button> */}
+            <button onClick={() => handleExt(message)} className="btn btn-success">Exit</button>
+            <button onClick={handleChng} className="btn btn-warning">Change</button>
+            <p>{message}</p>
         </div>
     )
 };
