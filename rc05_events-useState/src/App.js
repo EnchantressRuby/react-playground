db01a8ee1418265490e0810c51406cd0ef0db181
Ruby <rmyskhrmn584@gmail.com>
2024-09-02 20:05:53 +0200

    p

--- rc05_events-useState/src/App.js
+++ rc05_events-useState/src/App.js
@@ -1,7 +1,11 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Event from './components/1-events/Event';
+
 function App() {
   return (
-    <div className="App">
-      <h1>Cohort 17</h1>
+    <div className="App text-center">
+      <h1>Events - useState Hook</h1>
+      <Event />
     </div>
   );
 }
