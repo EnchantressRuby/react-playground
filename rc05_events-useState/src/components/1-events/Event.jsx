2f5a5b783a94519c83f198241bfcfebcaeb32e58
Ruby <rmyskhrmn584@gmail.com>
2024-09-02 20:15:29 +0200

    event

--- rc05_events-useState/src/components/1-events/Event.jsx
+++ rc05_events-useState/src/components/1-events/Event.jsx
@@ -1,7 +1,17 @@
 const Event = () => {
+
+    //? Event handler func
+
+    const handleClick = (event) => {
+        alert("Clicked!")
+        console.log(event);
+    }
+    
     return (
         <div>
-        <h2>Events</h2>
+            <h2>Events</h2>
+            <button onClick={handleClick} id="myBtn" name="click" className="btn btn-primary">Click me</button>
+            <button onClick={()=>alert("Deleted!")} className="btn btn-danger">Delete</button>
         </div>
     )
 };
