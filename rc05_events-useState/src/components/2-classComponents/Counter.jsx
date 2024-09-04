1049c8aecea31c53f92201156c55771cb7ca3ff3
Ruby <rmyskhrmn584@gmail.com>
2024-09-04 19:36:43 +0200

    class components examples

--- rc05_events-useState/src/components/2-classComponents/Counter.jsx
+++ rc05_events-useState/src/components/2-classComponents/Counter.jsx
@@ -5,12 +5,29 @@ class Counter extends Component {
         super(props)
         // state starting value
         this.state = { count: 0 }
+
+        // connecting the dec method to the counter class(bind)
+        this.decrement = this.decrement.bind(this)
     }
+
+    increment = () => {
+        this.setState
+            ({ count: this.state.count + 1 })
+    }
+
+    decrement() {
+        this.setState({ count: this.state.count - 1 })
+    }
+
     render() {
         return (
             <div>
                 <h2>Class Component</h2>
                 <h3>Count: {this.state.count}</h3>
+                <button onClick={this.increment} className="btn btn-primary">Increase</button>
+                <button onClick={()=>this.setState({count: 0})} className="btn btn-warning">Reset</button>
+                <button onClick={this.decrement} className="btn btn-danger">Decrease</button>
+                {/* <button onClick={this.decrement.bind(this)} className="btn btn-danger">Decrease</button> */}
             </div>
         )
     }
