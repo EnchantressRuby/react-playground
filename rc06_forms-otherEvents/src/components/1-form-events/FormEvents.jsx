d98fafe5698de8a1c3618ea5e640da5b3b45e6c8
Ruby <rmyskhrmn584@gmail.com>
2024-09-05 21:28:31 +0200

    p

--- rc06_forms-otherEvents/src/components/1-form-events/FormEvents.jsx
+++ rc06_forms-otherEvents/src/components/1-form-events/FormEvents.jsx
@@ -3,6 +3,8 @@ import React, { useState } from 'react';
 const FormEvents = () => {
     const [name, setName] = useState("")
     //? After each update of the state, the component will re-render and the changed parts are printed to the DOM
+    const [password, setPassword] = useState("")
+    const [country, setCountry] = useState("")
     return (
         <div className="mt-4 p-3">
             <div className="text-center">
@@ -12,7 +14,7 @@ const FormEvents = () => {
             <form>
                 <div className="mb-3">
                     <label htmlFor="name" className="form-label">
-                        Name
+                        Name : <span className='text-danger'>{name}</span> 
                     </label>
                     {/*onChange event is triggered whenever the input value changes
                     state is updated via the event written*/}
@@ -23,18 +25,18 @@ const FormEvents = () => {
                 </div>
                 <div className="mb-3">
                     <label htmlFor="password" className="form-label">
-                        Password
+                        Password: <span className='text-danger'>{password}</span>
                     </label>
-                    <input
+                    <input onChange={(e)=>setPassword(e.target.value)}
                         type="password"
                         className="form-control"
                         id="exampleInputPassword1" />
                 </div>
                 <div className="mb-3">
                     <label htmlFor="country" className="form-label">
-                        Country
+                        Country : <span className='text-danger'>{country}</span>
                     </label>
-                    <select id="country" className="form-select" aria-label="Default select example">
+                    <select id="country" className="form-select" aria-label="Default select example" onChange={(e)=>setCountry(e.target.value)}>
                         <option selected="">Countries</option>
                         <option value="Cze">Czech Republic</option>
                         <option value="Tr">Turkiye</option>
