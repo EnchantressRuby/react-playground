313dcf2965f901df261e2a7119fecc980026fe45
Ruby <rmyskhrmn584@gmail.com>
2024-09-07 21:40:23 +0200

    mouse events

--- rc06_forms-otherEvents/src/components/2-mouse-events/MouseEvents.jsx
+++ rc06_forms-otherEvents/src/components/2-mouse-events/MouseEvents.jsx
@@ -1,20 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const MouseEvents = () => {
+  const [visible, setVisible] = useState(false);
+  const handleDoubleClick = (e) => e.target.classList.toggle("bg-danger");
+  const [coordX, setCoordX] = useState("")
+  const [coordY, setCoordY] = useState("")
+  const handleMouseMove = (e) => {
+    //? absolute coordinates
+    // console.log("X:", e.pageX);
+    // console.log("Y:", e.pageY);
+    // console.log("X:", e.clientX);
+    // console.log("Y:", e.clientY);
+
+    //? relative coords
+    setCoordX(e.nativeEvent.offsetX);
+    setCoordY(e.nativeEvent.offsetY);
+  };
+
   return (
     <div className="container text-center d-flex flex-column align-items-center mt-4">
-        <h2 className="text-danger">Mouse Events</h2>
+      <h2 className="text-danger">Mouse Events</h2>
 
-        <div id='todo-1' className="bg-success text-light w-50 p-4 mt-4">onMouseOver & onMouseLeave</div>
+      <div onMouseOver={() => setVisible(true)}
+        onMouseLeave={() => setVisible(false)} id='todo-1' className="bg-success text-light w-50 p-4 mt-4">onMouseOver & onMouseLeave</div>
+      {visible && <p>Mouse on me</p>}
 
-        <div id='todo-2' className="bg-success text-light w-50 p-4 mt-4">onDoubleClick</div>
+      <div onDoubleClick={handleDoubleClick} id='todo-2' className="bg-success text-light w-50 p-4 mt-4">onDoubleClick</div>
 
-        <div id='todo-3' className="bg-success text-light w-50 p-4 mt-4"><span>onMouseMove</span> </div>
+      <div onMouseMove={handleMouseMove} id='todo-3' className="bg-success text-light w-50 p-4 mt-4"><span>onMouseMove</span> </div>
 
-        <div id='todo-4' className="bg-success text-light w-50 p-4 mt-4"><span>onMouseOver</span></div>
+      <div id='todo-4' className="bg-success text-light w-50 p-4 mt-4"><span>onMouseOver</span></div>
 
-        <div id='todo-5' className="bg-success text-light w-50 p-4 mt-4"><span>onMouseEnter</span></div>
+      <div id='todo-5' className="bg-success text-light w-50 p-4 mt-4"><span>onMouseEnter</span></div>
 
+      X: {coordX} and Y: {coordY}
     </div>
   )
 }
