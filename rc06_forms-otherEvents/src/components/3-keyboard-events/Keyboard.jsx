806ce18909f0b8f023e4845eeeafd187517c2dd7
Ruby <rmyskhrmn584@gmail.com>
2024-09-07 22:03:58 +0200

    keyboard events

--- rc06_forms-otherEvents/src/components/3-keyboard-events/Keyboard.jsx
+++ rc06_forms-otherEvents/src/components/3-keyboard-events/Keyboard.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
 
 const Keyboard = () => {
+
+    const handleKeyDown = (e) => {
+        console.log(e.keyCode);
+        console.log(e.key);
+        if (e.key ==="Enter") {
+            alert("Form submitted!")
+        }
+        if (e.key ==="Backspace") {
+            alert("Are you sure you want to delete it?")
+        }
+    }
+
     return (
         <div className="container mt-4">
             <h2>Keyboard Events</h2>
 
-            <input type="text" className='form-control' />
+            <input onKeyDown={handleKeyDown} type="text" className='form-control' />
 
             <div className="mt-3">
                 <p>{ }</p>
