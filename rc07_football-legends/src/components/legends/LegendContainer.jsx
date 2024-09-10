ed5e170c635791546fe52bf46fe71a1c6005e0c1
Ruby <rmyskhrmn584@gmail.com>
2024-09-10 20:55:30 +0200

    p

--- rc07_football-legends/src/components/legends/LegendContainer.jsx
+++ rc07_football-legends/src/components/legends/LegendContainer.jsx
@@ -3,26 +3,34 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Card from 'react-bootstrap/Card';
 import { data } from "../../helpers/data";
+import { useState } from "react";
 
 const LegendContainer = () => {
+    const [show, setShow] = useState(true)
     return (
         <Container>
             <Row>
                 {data.map(legend =>
                     <Col xs={10} sm={8} md={6} lg={4} xl={3}>
                         <Card>
-                            <Card.Img variant="top" src={legend.img}/>
-                            <Card.Body>
-                                <Card.Title>{legend.name}</Card.Title>
-                                <Card.Text>
-                                    <ul className="m-auto w-100">
-                                        {legend.statistics.map(item=><li className="list-unstyled h5 text-start">⚽{item}</li>)}
-                                    </ul>
-                                </Card.Text>
-                            </Card.Body>
-                            <Card.Footer>
-                                Career Years: {legend.official_career}
-                            </Card.Footer>
+                            {
+                                show ? <Card.Img onClick={()=> setShow(false)} variant="top" src={legend.img} /> :
+                                    <>
+                                        <Card.Body onClick={()=>setShow(true)}>
+                                            <Card.Title>{legend.name}</Card.Title>
+                                            <Card.Text>
+                                                <ul className="m-auto w-100">
+                                                    {legend.statistics.map(item => <li className="list-unstyled h5 text-start">⚽{item}</li>)}
+                                                </ul>
+                                            </Card.Text>
+                                        </Card.Body>
+                                        <Card.Footer>
+                                            Career Years: {legend.official_career}
+                                        </Card.Footer>
+                                    </>
+                            }
+
+
                         </Card>
                     </Col>)}
             </Row>
