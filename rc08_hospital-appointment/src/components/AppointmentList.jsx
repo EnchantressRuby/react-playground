ce147948b55df4e038153ec14f864e7a04ddcbe7
Ruby <rmyskhrmn584@gmail.com>
2024-09-14 22:02:46 +0200

    p

--- rc08_hospital-appointment/src/components/AppointmentList.jsx
+++ rc08_hospital-appointment/src/components/AppointmentList.jsx
@@ -3,7 +3,7 @@ import Container from "react-bootstrap/Container"
 const AppointmentList = () => {
     return (
         <Container className="p-2">
-            <h3 className="display-6 mb-2" style={{color:"rgb(166,18,189)"}}>
+            <h3 className="display-6 mb-2" style={{ color: "rgb(166,18,189)" }}>
                 Appointment List
             </h3>
         </Container>
