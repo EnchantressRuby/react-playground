3c1a8c90ddb35f3e102b01fd1308674b3efe277d
Ruby <rmyskhrmn584@gmail.com>
2024-09-16 20:21:03 +0200

    container

--- rc09_lifeCycle/src/App.js
+++ rc09_lifeCycle/src/App.js
@@ -1,9 +1,13 @@
-function App() {
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css"
+
+const App = () => {
   return (
-    <div className="App">
-      <h1>First App</h1>
+    <div className="text-center mt-4 container">
+      <button className="btn btn-danger">SHOW</button>
     </div>
   );
-}
+};
 
 export default App;
