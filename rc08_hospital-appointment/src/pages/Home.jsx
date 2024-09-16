c34e9f31abdb18d432dc92e6f36809abc2fe39de
Ruby <rmyskhrmn584@gmail.com>
2024-09-16 19:46:48 +0200

    local storage

--- rc08_hospital-appointment/src/pages/Home.jsx
+++ rc08_hospital-appointment/src/pages/Home.jsx
@@ -8,22 +8,26 @@ const Home = () => {
 
     const handleAdd = (newAppointment) => {
         setAppointments([...appointments, newAppointment])
-    }
+        localStorage.setItem("list", JSON.stringify([...appointments, newAppointment]))
+    };
 
     const handleDelete = (id) => {
         const filteredList = appointments.filter(item => item.id !== id);
         setAppointments(filteredList);
-    }
+        localStorage.setItem("list", JSON.stringify(filteredList))
+    };
 
     const handleDoubleClick = (id) => {
-        const updatedList = appointments.map(app => app.id === id ? { ...app, consulted: !app.consulted } : item)
-    }
+        const updatedList = appointments.map(app => app.id === id ? { ...app, consulted: !app.consulted } : app);
+        setAppointments(updatedList);
+        localStorage.setItem("list", JSON.stringify(updatedList))
+    };
 
     return (
         <main className="text-center mt-2">
             <h1 className="display-5 text-danger">ENC Hospital</h1>
             <Doctors handleAdd={handleAdd} />
-            <AppointmentList appointments={appointments} handleDelete={handleDelete} />
+            <AppointmentList appointments={appointments} handleDelete={handleDelete} handleDoubleClick={handleDoubleClick}/>
         </main>
     )
 }
