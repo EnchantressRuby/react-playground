eca10706a6c7d0e8770af9f6d56f0b960c66833c
Ruby <rmyskhrmn584@gmail.com>
2024-09-17 19:03:57 +0200

    update comp

--- rc09_lifeCycle/src/Clock.jsx
+++ rc09_lifeCycle/src/Clock.jsx
@@ -28,16 +28,27 @@ import { useState, useEffect } from "react"
 const Clock = () => {
 
     const [time, setTime] = useState(moment())
+    const [counter, setCounter] = useState(0)
 
     useEffect(() => {
         const TIME = setInterval(() => {
             setTime(moment())
         }, 1000);
+
+        //! closing comp
+        return () => {
+            clearInterval(TIME)
+        }
     }, [])
+
+    //! update comp
 
     return (
         <div>
             {time.format("HH")}:{time.format("mm")}:{time.format("ss")}
+            <h1 className="text-danger">Counter: {counter}</h1>
+            <button className="btn btn-secondary"
+                onClick={() => setCounter(counter + 1)}>Inc</button>
         </div>
     )
 }
