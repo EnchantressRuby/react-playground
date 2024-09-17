d470d6f641acf624ad087d66d3b63e227e6b5731
Ruby <rmyskhrmn584@gmail.com>
2024-09-17 18:38:16 +0200

    clock comp

--- rc09_lifeCycle/src/App.js
+++ rc09_lifeCycle/src/App.js
@@ -2,13 +2,16 @@ import React, { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css"
 import LifeCycleMethods from "./LifeCycleMethods";
+import Clock from "./Clock";
 
 const App = () => {
   const [show, setShow] = useState(true)
   return (
     <div className="text-center mt-4 container">
       <button onClick={() => setShow(!show)} className="btn btn-danger">SHOW</button>
-      {show && <LifeCycleMethods />}
+      {/* {show && <LifeCycleMethods />} */}
+      {show && <Clock />}
+
     </div>
   );
 }
