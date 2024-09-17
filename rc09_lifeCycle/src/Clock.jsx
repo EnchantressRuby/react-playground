9dea9e2428cb0d57ae0ff11807dddb9789aa6cb7
Ruby <rmyskhrmn584@gmail.com>
2024-09-17 18:47:02 +0200

    p

--- rc09_lifeCycle/src/Clock.jsx
+++ rc09_lifeCycle/src/Clock.jsx
@@ -1,4 +1,28 @@
-import React from 'react'
+// //! useEffect(() => {
+// //*   /* ComponentDidMount code */
+// //! }, []);
+
+// //! useEffect(() => {
+// //*   */ componentDidUpdate code */
+// //! }, [var1, var2]);
+
+// //! useEffect(() => {
+
+// //!   return () => {
+// //*     //* componentWillUnmount code */
+// //!   };
+// //! }, []);
+
+// //! useEffect(() => {
+// //*   //* componentDidMount code + componentDidUpdate code */
+
+// //!   return () => {
+// //*     //* componentWillUnmount code */
+// //!   };
+// //! }, [var1, var2]);
+
+
+import moment from "moment"
 
 const Clock = () => {
   return (
