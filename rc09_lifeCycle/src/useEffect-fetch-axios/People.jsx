e8b71c53080abaae518574003caee9cd7cebaf3a
Ruby <rmyskhrmn584@gmail.com>
2024-09-19 21:23:13 +0200

    p

--- rc09_lifeCycle/src/useEffect-fetch-axios/People.jsx
+++ rc09_lifeCycle/src/useEffect-fetch-axios/People.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 
 const People = () => {
 
-  const [People, setPeople] = useState([])
+  const [people, setPeople] = useState([])
 
   //! 1- fetch, then
 
@@ -15,7 +15,19 @@ const People = () => {
   console.log("hello");
 
   return (
-    <div>People</div>
+    <div className="container text-center mt-4">
+
+      <div className="row">
+        {people.map((a) => (
+          <div className="col-12 col-sm-6 col-md-4">
+            <img src={a.avatar} alt="" />
+            <h5>{a.first_name}</h5>
+            <h6>{a.last_name}</h6>
+          </div>
+        ))}
+      </div>
+
+    </div>
   )
 }
 
