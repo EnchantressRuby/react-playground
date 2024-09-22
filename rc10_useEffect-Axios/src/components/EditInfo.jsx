84b1f3c9dcc177f951fcde8fc714701b24a480b0
Ruby <rmyskhrmn584@gmail.com>
2024-09-22 20:51:41 +0200

    edit info comp

--- rc10_useEffect-Axios/src/components/EditInfo.jsx
+++ rc10_useEffect-Axios/src/components/EditInfo.jsx
@@ -14,22 +14,42 @@ const EditInfo = () => {
                             id="exampleModalLabel">
                             Modal
                         </h1>
-                        <button className="btn-close"
+                        <button
+                            className="btn-close"
                             type="button"
                             data-bs-dismiss="modal"
                             aria-label="Close"></button>
                     </div>
                     <div className="modal-body">
                         <div className="mb-3">
-                            <label htmlFor="title"
+                            <label
+                                htmlFor="title"
                                 className="form-label">
                                 Title
                             </label>
                             <input
                                 type="text" className="form-control"
                                 id="title"
-                                placeholder="Enter your title"
-                            />
+                                placeholder="Enter your title" />
+                        </div>
+                        <div className="mb-3">
+                            <label
+                                htmlFor="desc"
+                                className="form-label">
+                                Description
+                            </label>
+                            <input
+                                type="text" className="form-control"
+                                id="desc"
+                                placeholder="Enter your description" />
+                        </div>
+                        <div className="modal-footer">
+                            <button
+                                className="btn btn-secondary"
+                                type="button"
+                                data-bs-dismiss="modal">
+                                Save
+                            </button>
                         </div>
                     </div>
                 </div>
@@ -37,3 +57,5 @@ const EditInfo = () => {
         </div>
     )
 }
+
+export default EditInfo
