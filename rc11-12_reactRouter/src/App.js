54211f7aff932aa29fe1c8b765f878d07c63b5e3
Ruby <rmyskhrmn584@gmail.com>
2024-09-23 22:25:04 +0200

    not found comp

--- rc11-12_reactRouter/src/App.js
+++ rc11-12_reactRouter/src/App.js
@@ -1,16 +1,19 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom"
 import "bootstrap/dist/css/bootstrap.min.css"
-import Home from "./pages/Home";
-import MyNavbar from "./components/MyNavbar";
-import Teacher from "./pages/Teacher"
-import CourseCard from "./pages/CourseCard";
-import ContactForm from "./pages/ContactForm";
-import Paths from "./pages/Paths"
+// import { BrowserRouter, Route, Routes } from "react-router-dom"
+// import Home from "./pages/Home";
+// import MyNavbar from "./components/MyNavbar";
+// import Teacher from "./pages/Teacher"
+// import CourseCard from "./pages/CourseCard";
+// import ContactForm from "./pages/ContactForm";
+// import Paths from "./pages/Paths"
+
+import AppRouter from "./router/AppRouter"
 
 function App() {
   return (
     <div>
-      <BrowserRouter>
+      <AppRouter />
+      {/* <BrowserRouter>
         <MyNavbar />
         <Routes>
           <Route path="/" element={<Home />} />
@@ -19,7 +22,7 @@ function App() {
           <Route path="/contact" element={<ContactForm />} />
           <Route path="/paths" element={<Paths />} />
         </Routes>
-      </BrowserRouter>
+      </BrowserRouter> */}
     </div>
   );
 }
--- rc11-12_reactRouter/src/router/AppRouter.jsx
+++ rc11-12_reactRouter/src/router/AppRouter.jsx
@@ -1,30 +1,31 @@
 import "bootstrap/dist/css/bootstrap.min.css"
-// import { BrowserRouter, Route, Routes } from "react-router-dom"
-// import Home from "./pages/Home";
-// import MyNavbar from "./components/MyNavbar";
-// import Teacher from "./pages/Teacher"
-// import CourseCard from "./pages/CourseCard";
-// import ContactForm from "./pages/ContactForm";
-// import Paths from "./pages/Paths"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Home from "../pages/Home";
+import MyNavbar from "../components/MyNavbar";
+import Teacher from "../pages/Teacher"
+import CourseCard from "../pages/CourseCard";
+import ContactForm from "../pages/ContactForm";
+import Paths from "../pages/Paths"
+import NotFound from "../pages/NotFound"
 
-import AppRouter from "./router/AppRouter"
-
-function App() {
+const AppRouter =() => {
     return (
         <div>
-            {/* <BrowserRouter>
-        <MyNavbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/teacher" element={<Teacher />} />
-          <Route path="/courses" element={<CourseCard />} />
-          <Route path="/contact" element={<ContactForm />} />
-          <Route path="/paths" element={<Paths />} />
-        </Routes>
-      </BrowserRouter> */}
-            <AppRouter />
+            <BrowserRouter>
+                <MyNavbar />
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/teacher" element={<Teacher />} />
+                    <Route path="/courses" element={<CourseCard />} />
+                    <Route path="/contact" element={<ContactForm />} />
+                    <Route path="/paths" element={<Paths />} />
+
+                    <Route path="" element={<NotFound/>} />
+
+                </Routes>
+            </BrowserRouter>
         </div>
     );
 }
 
-export default App;
+export default AppRouter;
