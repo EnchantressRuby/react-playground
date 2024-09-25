c47f37263c5281c9917f8640930ac8b3419f58e2
Ruby <rmyskhrmn584@gmail.com>
2024-09-25 20:40:30 +0200

    context consumer

--- rc13-Context-Reducer/context-1/src/components/StudentItem.jsx
+++ rc13-Context-Reducer/context-1/src/components/StudentItem.jsx
@@ -5,10 +5,18 @@ import { StudentContext } from '../App'
 const StudentItem = () => {
 
     //! context consumer
-    const { students } = useContext()
+    const { students } = useContext(StudentContext)
     return (
         <div>
-
+            {students.map((std)=>(
+                <div key={std.id} style={{background:std.color}}>
+                    <h3>Name: {std.name}</h3>
+                    <h4>E-Mail: {std.email}</h4>
+                    <h4>Age: {std.age}</h4>
+                    Color: <input type="text" value={std.color}
+                    onChange={(e)=>changeColor(std.id,e.target.value)}/>
+                </div>
+            ))}
         </div>
     )
 }
