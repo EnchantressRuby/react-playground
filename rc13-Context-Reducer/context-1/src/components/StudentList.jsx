f040e2d1f12dfd36991e172ad5a2ca95f60f28b2
Ruby <rmyskhrmn584@gmail.com>
2024-09-25 20:24:57 +0200

    student list style

--- rc13-Context-Reducer/context-1/src/components/StudentList.jsx
+++ rc13-Context-Reducer/context-1/src/components/StudentList.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 import StudentItem from "./StudentItem";
 const StudentList = () => {
   return (
-    <div>
+    <div style={{textAlign:"center"}}>
       <img
         src="https://gelecekegitimde.com/wp-content/uploads/2015/01/fft99_mf3019637.jpeg"
         alt=""
       />
 
-      <h2 style={{ textAlign: "center", background: "pink" }}>Student List</h2>
+      <h2 style={{background: "pink" }}>Student List</h2>
 
       <StudentItem />
     </div>
