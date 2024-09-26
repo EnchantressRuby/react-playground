2df61c87a257338b49d641259ad81b0ce15cd89b
Ruby <rmyskhrmn584@gmail.com>
2024-09-26 20:54:53 +0200

    navbar and dark mode

--- rc14_tailwindCSS/src/App.js
+++ rc14_tailwindCSS/src/App.js
@@ -2,11 +2,13 @@ import Typography from "./components/Typography"
 import SpacingAndSizing from "./components/SpacingAndSizing"
 import HoverAndFocus from "./components/HoverAndFocus";
 import FlexAndResponsive from "./components/FlexAndResponsive";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div>
-      <h1 className="text-2xl text-center text-purple-700 font-bold">
+    <div className="dark:bg-gray-600 min-h-screen">
+      <Navbar />
+      <h1 className="text-2xl text-center text-purple-700 dark:text-white font-bold">
         Hi Tailwind
       </h1>
       <FlexAndResponsive />
