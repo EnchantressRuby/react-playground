27f4cdf0a398b9ef12bf0da03641fb9e42e7c829
Ruby <rmyskhrmn584@gmail.com>
2024-09-26 19:37:17 +0200

    typography

--- rc14_tailwindCSS/src/components/Typography.jsx
+++ rc14_tailwindCSS/src/components/Typography.jsx
@@ -20,6 +20,21 @@ const Typography = () => {
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum, neque voluptatem voluptatum incidunt provident nihil! Voluptatum, asperiores. Cumque in sapiente voluptatibus labore et, reiciendis dolorum eligendi aliquid, non esse dolorem quo itaque a unde earum suscipit dolores perferendis error deleniti ullam. Necessitatibus ipsam quasi tempora modi adipisci corporis ad vitae!
                 </p>
             </article>
+            <div className='font-bold'>
+                <p className='font-bold'>Hello</p>
+            <p className='text-center'>Hello</p>
+            <p className='underline'>Hello</p>
+            <p className='line-through'>Hello</p>
+            <p className='uppercase'>Hello</p>
+            <p className='lowercase'>Hello</p>
+            <p className='text-red-700'>Hello</p>
+            <p className='text-red-950'>Hello</p>
+            <p className='text-[#D2E0FB]'>Hello</p>
+            <p className='text-[rgb(254,249,217)]'>Hello</p>
+            <p className='bg-slate-500'>Hello</p>
+            <p className='bg-[#D2E0FB]'>Hello</p>
+            </div>
+            
         </div>
     )
 }
