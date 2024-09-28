5288386341f801f54f12e5667e8769e97469cfb0
Ruby <rmyskhrmn584@gmail.com>
2024-09-28 16:06:29 +0200

    navbar style

--- rc15-16_Portfolio-Building/src/components/Navbar.jsx
+++ rc15-16_Portfolio-Building/src/components/Navbar.jsx
@@ -1,6 +1,21 @@
 import React, { useState } from "react"
 import logo from "../assets/logo.png"
 import { closeNavbar, openNavbar } from "../helper/icons"
+import { NavLink } from "react-router-dom";
+const navigation = [
+  {
+    title: "Home",
+    path: "/dashboard"
+  },
+  {
+    title: "Products",
+    path: "/dashboard/products"
+  },
+  {
+    title: "About",
+    path: "/dashboard/about"
+  }
+];
 
 const Navbar = () => {
   const [show, setshow] = useState(false)
@@ -21,6 +36,16 @@ const Navbar = () => {
               {show ? closeNavbar : openNavbar}
             </button>
           </div>
+        </div>
+
+        <div className={`${show ? "flex flex-col pb-2" : "hidden"}md:flex md:flex-row flex-1 items-center`}>
+          <ul className="md:flex md:space-x-6">
+            {navigation.map((item) => (
+              <li className="text-gray-700 font-medium flex justify-center" key={item.title}>
+                <NavLink className={`block hover:bg-main rounded-full py-2 px-4 hover:text-white`} to={item.path}> {item.title} </NavLink>
+              </li>
+            ))}
+          </ul>
         </div>
       </div>
     </nav>
