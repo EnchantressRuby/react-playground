5c4ffcc27c46fe84c170e9a7182ba9cbb52818f9
Ruby <rmyskhrmn584@gmail.com>
2024-10-02 20:01:18 +0200

    context provider

---
+++ rc17-18_Movie-App/src/context/AuthProvider.jsx
@@ -0,0 +1,19 @@
+import React, { createContext, useContext } from 'react'
+
+export const AuthContext = createContext()
+
+//*with custom hook
+export const useAuthContext = () => {
+    return useContext(AuthContext)
+}
+
+const AuthProvider = ({ children }) => {
+    const values = {}
+    return (
+        <AuthContext.Provider value={values}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
+
+export default AuthProvider
