577dc28a02870d3d9603e1d0d3477ee286841a80
Ruby <rmyskhrmn584@gmail.com>
2024-10-04 21:11:47 +0200

    button & box

--- rc19_Material-UI/src/components/MuiElem.jsx
+++ rc19_Material-UI/src/components/MuiElem.jsx
@@ -1,9 +1,36 @@
+// import { Container } from '@mui/material'
+// performance problems only
+
+import { Typography } from '@mui/material'
+import Container from '@mui/material/Container'
+// better performance
+
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+
 import React from 'react'
 
 const MuiElem = () => {
-  return (
-    <div>MuiElem</div>
-  )
+    return (
+        <Container maxWidth="x1">
+            <Typography align='center' color='success' variant='h4' variantMapping={{ "h4": "h2" }}>
+                Mui Elements
+            </Typography>
+            <Typography align='center' color='error' variant='h4' component="h3" mt={5}>
+                Mui Elements
+            </Typography>
+            <Typography align='center' color='white' variant='body1' mt={5} sx={{ border: "3px solid black", backgroundColor: "maroon" }}>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste deleniti nisi consequuntur magni facere nesciunt voluptatibus, maxime nobis dicta, ipsam ullam quia sapiente illum? Perferendis.
+            </Typography>
+
+            <Box sx={{ display: "flex", justifyContent: "center", alignItems: "center", gap: 3, marginTop: 3 }}>
+                <Button variant="text">Text</Button>
+                <Button variant="contained">Contained</Button>
+                <Button variant="outlined">Outlined</Button>
+            </Box>
+
+        </Container>
+    )
 }
 
 export default MuiElem
