42a1f69b3e4733c49dcd94648a5d698996c5e62a
Ruby <rmyskhrmn584@gmail.com>
2024-10-04 21:16:24 +0200

    stack

--- rc19_Material-UI/src/components/MuiElem.jsx
+++ rc19_Material-UI/src/components/MuiElem.jsx
@@ -7,19 +7,35 @@ import Container from '@mui/material/Container'
 
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
+import Stack from '@mui/material/Stack';
+
 
 import React from 'react'
 
 const MuiElem = () => {
     return (
         <Container maxWidth="x1">
-            <Typography align='center' color='success' variant='h4' variantMapping={{ "h4": "h2" }}>
+            <Typography
+                align='center'
+                color='success'
+                variant='h4'
+                variantMapping={{ "h4": "h2" }}>
                 Mui Elements
             </Typography>
-            <Typography align='center' color='error' variant='h4' component="h3" mt={5}>
+            <Typography
+                align='center'
+                color='error'
+                variant='h4'
+                component="h3"
+                mt={5}>
                 Mui Elements
             </Typography>
-            <Typography align='center' color='white' variant='body1' mt={5} sx={{ border: "3px solid black", backgroundColor: "maroon" }}>
+            <Typography
+                align='center'
+                color='white'
+                variant='body1'
+                mt={5}
+                sx={{ border: "3px solid black", backgroundColor: "maroon" }}>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste deleniti nisi consequuntur magni facere nesciunt voluptatibus, maxime nobis dicta, ipsam ullam quia sapiente illum? Perferendis.
             </Typography>
 
@@ -29,11 +45,26 @@ const MuiElem = () => {
                 <Button variant="outlined">Outlined</Button>
             </Box>
 
-            <Box display="flex" justifyContent={"center"} alignItems="center" gap={3} marginTop={3}>
+            <Box
+                display="flex"
+                justifyContent={"center"}
+                alignItems="center"
+                gap={3}
+                marginTop={3}>
                 <Button variant="text">Text</Button>
                 <Button variant="contained">Contained</Button>
                 <Button variant="outlined">Outlined</Button>
             </Box>
+
+            <Stack
+                direction={{ xs: 'column', sm: 'row' }}
+                spacing={{ xs: 1, sm: 2, md: 4 }}>
+                <Button variant="text">Text</Button>
+                <Button variant="contained">Contained</Button>
+                <Button variant="outlined">Outlined</Button>
+            </Stack>
+
+
 
         </Container >
     )
