3c15f2aa7eb097b6df97356df4118f4d23e68365
Ruby <rmyskhrmn584@gmail.com>
2024-10-06 16:48:24 +0200

    form control

--- rc19_Material-UI/src/components/MuiInputElem.jsx
+++ rc19_Material-UI/src/components/MuiInputElem.jsx
@@ -2,15 +2,32 @@ import { Container } from '@mui/material'
 import React from 'react'
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
+import Input from '@mui/material/Input';
+import InputLabel from '@mui/material/InputLabel';
+import InputAdornment from '@mui/material/InputAdornment';
+import FormControl from '@mui/material/FormControl';
 
 const MuiInputElem = () => {
     return (
         <Container>
-            <Box width={500} display="flex" justifyContent="center">
-                <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-                <TextField id="filled-basic" label="Filled" variant="filled" />
-                <TextField id="standard-basic" label="Standard" variant="standard" />
+            <Box width={500} display="flex" justifyContent="center" flexDirection={"column"}>
+                <TextField type='email' id="outlined-basic" margin='dense' label="Outlined" variant="outlined" fullWidth />
+                <TextField id="filled-basic" margin='dense' label="Filled" variant="filled" fullWidth />
+                <TextField id="standard-basic" margin='dense' label="Standard" variant="standard" fullWidth />
             </Box>
+            <FormControl variant="standard">
+        <InputLabel htmlFor="input-with-icon-adornment">
+          With a start adornment
+        </InputLabel>
+        <Input
+          id="input-with-icon-adornment"
+          startAdornment={
+            <InputAdornment position="start">
+              <AccountCircle />
+            </InputAdornment>
+          }
+        />
+      </FormControl>
         </Container>
     )
 }
