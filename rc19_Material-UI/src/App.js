2243ef79ce8e13222be78f6d61c338131febd59d
Ruby <rmyskhrmn584@gmail.com>
2024-10-06 17:35:46 +0200

    customize theme

--- rc19_Material-UI/src/App.js
+++ rc19_Material-UI/src/App.js
@@ -1,15 +1,32 @@
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import Home from './pages/Home';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { green, purple } from '@mui/material/colors';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: purple[500],
+    },
+    secondary: {
+      main: green[500],
+    },
+  },
+});
+
 
 function App() {
   return (
-    <Container maxWidth={"x1"}>
-      <Typography variant="h3" component="h1" gutterBottom>
-        Hello MUI
-      </Typography>
-      <Home />
-    </Container>
+    <ThemeProvider theme={theme}>
+      <Container maxWidth={"x1"}>
+        <Typography variant="h3" component="h1" gutterBottom>
+          Hello MUI
+        </Typography>
+        <Home />
+      </Container>
+    </ThemeProvider>
+
   );
 }
 
