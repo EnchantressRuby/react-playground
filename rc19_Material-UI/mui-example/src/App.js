a9004ae521cd1565dc578db91418fa0db3a9c41b
Ruby <rmyskhrmn584@gmail.com>
2024-10-06 18:07:15 +0200

    appbar added

--- rc19_Material-UI/mui-example/src/App.js
+++ rc19_Material-UI/mui-example/src/App.js
@@ -1,8 +1,22 @@
+import Container from "@mui/material/Container"
+import Products from "./pages/Products";
+import { ThemeProvider } from "@emotion/react";
+import { createTheme } from "@mui/material";
+import CssBaseline from '@mui/material/CssBaseline';
+
+
+const theme = createTheme()
+
 function App() {
   return (
-    <div className="App">
-      <h1>First App</h1>
-    </div>
+    <ThemeProvider theme={theme}>
+      {/* reset.css */}
+      <CssBaseline />
+      <Container>
+        <Products />
+      </Container>
+    </ThemeProvider>
+
   );
 }
 
---
+++ rc19_Material-UI/mui-example/src/pages/Products.jsx
@@ -0,0 +1,13 @@
+import { Container } from '@mui/material'
+import React from 'react'
+import Navbar from '../components/Navbar'
+
+const Products = () => {
+  return (
+    <Container>
+        <Navbar/>
+    </Container>
+  )
+}
+
+export default Products
