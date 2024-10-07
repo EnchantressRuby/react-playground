f2482fc9d7229c2b4f1eb178a5de84fd8ce0814d
Ruby <rmyskhrmn584@gmail.com>
2024-10-07 18:13:35 +0200

    set

--- rc20_redux-intro/src/App.js
+++ rc20_redux-intro/src/App.js
@@ -1,9 +1,21 @@
-function App() {
+import React from 'react'
+import Counter from "./components/counter/Counter";
+import "./App.css";
+import { Provider } from "react-redux";
+import { store } from './redux/store';
+import Todo from "./components/todo/Todo";
+const App = () => {
   return (
-    <div className="App">
-      <h1>First App</h1>
+    <div className="app">
+      <Provider store={store}>
+
+        <Counter />
+
+<Todo/>
+
+      </Provider>
     </div>
   );
 }
 
-export default App;
+export default App
