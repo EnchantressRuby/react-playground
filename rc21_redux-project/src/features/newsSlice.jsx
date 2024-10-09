f2f57a7fef0a272c9691f38ff547cf5cd56f9340
Ruby <rmyskhrmn584@gmail.com>
2024-10-09 21:32:41 +0200

    newsSlice

--- rc21_redux-project/src/features/newsSlice.jsx
+++ rc21_redux-project/src/features/newsSlice.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
+import axios from "axios"
 
-const newsSlice = () => {
-  return (
-    <div>newsSlice</div>
-  )
-}
+export const getData = createAsyncThunk("newsSlice/getData", async () => {
+  const response = await axios(
+    "https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=81a4163ea7eb4bccb489151972100adb"
+  );
 
-export default newsSlice
+  return response.data.articles
+
+})
+
+export const newsSlice = createSlice({
+  name: "newsSlice",
+
+  initialState: {
+    news: [],
+    loading: true,
+  },
+
+  reducers: {
+    clearNews: () => {
+
+    }
+  },
+
+  extraReducers: (builder) => {
+    builder
+      .addCase(getData.pending, (state) => {
+        state.loading = true
+      })
+      .addCase(getData.fulfilled, (state, action) => {
+        state.news = action.payload
+        state.loading = false
+      })
+  },
+})
+
+
+export const { clearNews } = newsSlice.actions
+
+export default newsSlice.reducer
