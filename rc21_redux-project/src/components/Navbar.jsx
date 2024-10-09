127755f6cbf245d8d2f0be84bfddc0ac0d56af04
Ruby <rmyskhrmn584@gmail.com>
2024-10-09 21:07:47 +0200

    navbar

--- rc21_redux-project/src/components/Navbar.jsx
+++ rc21_redux-project/src/components/Navbar.jsx
@@ -1,9 +1,43 @@
 import React from 'react'
+import { AppBar, Box, Button, Toolbar, Typography } from "@mui/material";
+import { useDispatch, useSelector } from 'react-redux';
+import { deleteUser } from '../features/permissionSlice';
 
 const Navbar = () => {
+
+  const { email } = useSelector((state) => state.permissionSlice)
+
+  const dispatch = useDispatch()
+
+  const signOut = () => {
+
+    dispatch(deleteUser())
+
+  }
+
   return (
-    <div>Navbar</div>
-  )
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static" color="secondary">
+        <Toolbar>
+          <Typography
+            variant="h6"
+            component="div"
+            sx={{ flexGrow: 1, cursor: "pointer" }}
+          >
+            Kovach News
+          </Typography>
+
+          {email ? (
+            <Button color="inherit" onClick={signOut}>
+              LogOut
+            </Button>
+          ) : (
+            <Button color="inherit">LogIn</Button>
+          )}
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
 }
 
 export default Navbar
